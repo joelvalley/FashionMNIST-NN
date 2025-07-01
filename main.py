ff3149d224cd5a89ad26fc26f1c9780e5bc60569
Joel <joeldpv6@icloud.com>
2025-07-01 19:16:57 -0400

    Add baseline code

--- main.py
+++ main.py
@@ -17,3 +17,235 @@ from tqdm.auto import tqdm
 
 # Timer
 from timeit import default_timer as timer
+
+# Import helper functions
+from helper_functions import accuracy_fn
+
+# Get training & testing data
+train_data = datasets.FashionMNIST(
+    root="data", # Where to download data to?
+    train=True, # Do we want the training dataset?
+    download=True,  # Do we want to download yes/no?
+    transform=ToTensor(),   # How do we want to transform the data?
+    target_transform=None   # How do we want to transform the targets?
+)
+
+test_data = datasets.FashionMNIST(
+    root="data", # Where to download data to?
+    train=False, # Do we want the training dataset?
+    download=True,  # Do we want to download yes/no?
+    transform=ToTensor(),   # How do we want to transform the data?
+    target_transform=None   # How do we want to transform the targets?
+)
+
+# Save important features from the data
+class_names = train_data.classes
+torch.manual_seed(42)
+
+# Batchsize
+BATCH_SIZE = 32
+
+# Turn datasets into iterables (batches)
+train_dataloader = DataLoader(dataset=train_data,
+                              batch_size=BATCH_SIZE,
+                              shuffle=True)
+
+test_dataloader = DataLoader(dataset=test_data,
+                             batch_size=BATCH_SIZE,
+                             shuffle=False)
+
+train_features_batch, train_labels_batch = next(iter(train_dataloader))
+
+# Device agnostic code
+device = "mps" if torch.backends.mps.is_available() else "cpu"
+print(f"Using device: {device}")
+
+# Model
+class FashionMNISTModel(nn.Module):
+    def __init__(self,
+                 input_shape: int,
+                 hidden_units: int,
+                 output_shape: int):
+        super().__init__()
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(in_channels=input_shape,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2)
+        )
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.Conv2d(in_channels=hidden_units,
+                      out_channels=hidden_units,
+                      kernel_size=3,
+                      stride=1,
+                      padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2,
+                         stride=2)
+        )
+        self.classifier = nn.Sequential(
+            nn.Flatten(),
+            nn.Linear(in_features=hidden_units*7*7,  # Assuming input shape is 28x28
+                      out_features=output_shape)
+        )
+
+    def forward(self, x):
+        x = self.conv_block_1(x)
+        x = self.conv_block_2(x)
+        x = self.classifier(x)
+        return x
+
+# Setup model with input parameters
+model = FashionMNISTModel(
+    input_shape=1,    # 1 colour channel (grayscale)
+    hidden_units=10,    # Units in the hidden layer
+    output_shape=len(class_names)   # One for every class
+).to(device)
+
+# Loss and optimizer
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.SGD(params=model.parameters(),
+                            lr=0.1)
+
+# Timer function
+def print_train_time(start: float, end: float, device: torch.device = None):
+    """Prints the difference between the start and the end time"""
+    total_time = end - start
+    print(f"Train time on {device}: {total_time:.3f} seconds")
+    return total_time
+
+# Training step
+def train_step(model: torch.nn.Module,
+               data_loader: torch.utils.data.DataLoader,
+               loss_fn: torch.nn.Module,
+               optimizer: torch.optim.Optimizer,
+               accuracy_fn,
+               device: torch.device = device):
+    """Performs training step with model trying to learn on data_loader"""
+    train_loss, train_acc = 0, 0
+
+    # Put model in training mode
+    model.train()
+
+    # Add a loop to loop through the training batches
+    for batch, (X, y) in enumerate(data_loader):
+        X, y = X.to(device), y.to(device)
+
+        # 1. Forward pass (outputs the raw logits of the model)
+        y_logits = model(X)
+        y_pred = torch.argmax(torch.softmax(y_logits, dim=1), dim=1)
+
+        # 2. Calculate loss and accuracy per batch
+        loss = loss_fn(y_logits, y)
+        train_loss += loss  # Accumulate train loss
+        train_acc += accuracy_fn(y_true=y,
+                                 y_pred=y_pred)
+
+        # 3. Optimizer zero grad
+        optimizer.zero_grad()
+
+        # 4. Loss backward (backpropagation)
+        loss.backward()
+
+        # 5. Optimizer step (gradient descent)
+        optimizer.step()
+    
+    # Calculate training loss & train acc by length of dataloader
+    train_loss /= len(data_loader)
+    train_acc /= len(data_loader)
+    print(f"Train loss: {train_loss:.5f} | Train acc: {train_acc:.2f}%\n")
+
+# Testing step
+def test_step(model: torch.nn.Module, 
+              data_loader: torch.utils.data.DataLoader, 
+              loss_fn: torch.nn.Module, 
+              accuracy_fn,
+              device: torch.device = device):
+    """Performs a testing loop step on model going over data_loader"""
+    test_loss, test_acc = 0, 0
+
+    model.eval()
+    with torch.inference_mode():
+        for X, y in data_loader:
+            X, y = X.to(device), y.to(device)
+
+            # 1. Forward pass
+            test_logits = model(X)
+            test_pred = torch.argmax(torch.softmax(test_logits, dim=1), dim=1)
+            
+            # 2. Calculate loss
+            test_loss += loss_fn(test_logits, y)
+            test_acc += accuracy_fn(y_true=y, y_pred=test_pred)
+        
+        test_loss /= len(data_loader)
+        test_acc /= len(data_loader)
+        print(f"Test loss: {test_loss:.5f} | Test acc: {test_acc:.2f}%\n")
+
+def eval_model(model: torch.nn.Module, 
+               data_loader: torch.utils.data.DataLoader, 
+               loss_fn: torch.nn.Module, 
+               accuracy_fn,
+               device: torch.device = device):
+    """Returns a dictionary of the results of the model predicting on data_loader"""
+    model.to(device)
+    loss, acc = 0, 0
+    model.eval()
+    with torch.inference_mode():
+        for X, y in data_loader:
+            X, y = X.to(device), y.to(device)
+            y_pred = model(X)
+
+            loss += loss_fn(y_pred, y)
+            acc += accuracy_fn(y_true=y, y_pred=torch.argmax(torch.softmax(y_pred, dim=1), dim=1))
+
+        loss /=len(data_loader)
+        acc /=len(data_loader)
+    return {"model_name" : model.__class__.__name__,
+            "model_loss" : loss.item(),
+            "model_acc" : acc}
+
+train_time_start = timer()
+
+epochs = 3
+
+for epoch in tqdm(range(epochs)):
+    print(f"Epoch: {epoch}")
+    train_step(model=model,
+               data_loader=train_dataloader,
+               loss_fn=loss_fn,
+               optimizer=optimizer,
+               accuracy_fn=accuracy_fn,
+               device=device)
+    test_step(model=model,
+               data_loader=test_dataloader,
+               loss_fn=loss_fn,
+               accuracy_fn=accuracy_fn,
+               device=device)
+
+# Calculate training time
+train_time_end = timer()
+total_train_time = print_train_time(start=train_time_start, end=train_time_end, device=device)
+
+model_results = eval_model(model=model,
+                           data_loader=test_dataloader,
+                           loss_fn=loss_fn,
+                           accuracy_fn=accuracy_fn,
+                           device=device)
+
+print(model_results)

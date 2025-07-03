629927ab03c10c1c7db197e07205ad2c9356e3c5
Joel <joeldpv6@icloud.com>
2025-07-03 13:03:55 -0400

    add random testing, confusion matrix, and save path

--- main.py
+++ main.py
@@ -8,18 +8,28 @@ from torchvision import datasets
 from torchvision import transforms
 from torchvision.transforms import ToTensor
 
-# Import MatPlotLib
+# Import torchmetrics and mlxtend
+from torchmetrics import ConfusionMatrix
+from mlxtend.plotting import plot_confusion_matrix
+
+# Import MatPlotLib, Pandas and NumPy
 import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
 
 # Data loader
 from torch.utils.data import DataLoader
 from tqdm.auto import tqdm
 
-# Timer
+# Timer & random
 from timeit import default_timer as timer
+import random
 
 # Import helper functions
 from helper_functions import accuracy_fn
+
+# Import paths
+from pathlib import Path
 
 # Get training & testing data
 train_data = datasets.FashionMNIST(
@@ -40,7 +50,10 @@ test_data = datasets.FashionMNIST(
 
 # Save important features from the data
 class_names = train_data.classes
+
+# Set random seed for reproducibility
 torch.manual_seed(42)
+torch.mps.manual_seed(42)
 
 # Batchsize
 BATCH_SIZE = 32
@@ -220,6 +233,30 @@ def eval_model(model: torch.nn.Module,
             "model_loss" : loss.item(),
             "model_acc" : acc}
 
+def make_predictions(model: torch.nn.Module,
+                     data: list,
+                     device: torch.device = device):
+    model.to(device)
+
+    pred_probs = []
+    model.eval()
+    with torch.inference_mode():
+        for sample in data:
+            # Prepare the sample (add batch dimension and move to device)
+            sample = torch.unsqueeze(sample, dim=0).to(device)
+
+            # Forward pass (model outputs raw logits)
+            pred_logit = model(sample)
+
+            # Get prediction probability (logit -> pred probs)
+            pred_prob = torch.softmax(pred_logit.squeeze(), dim=0)
+
+            # Get pred_prob off the GPU for further calculations
+            pred_probs.append(pred_prob.cpu())
+
+    # Stack the pred_probs to turn list into a tensor
+    return torch.stack(pred_probs)
+
 train_time_start = timer()
 
 epochs = 3
@@ -248,4 +285,92 @@ model_results = eval_model(model=model,
                            accuracy_fn=accuracy_fn,
                            device=device)
 
-print(model_results)
+results = pd.DataFrame([model_results])
+print(results)
+
+# 1. Create model directory path
+MODEL_PATH = Path("models")
+MODEL_PATH.mkdir(parents=True, exist_ok=True)
+
+# 2. Create model save path
+MODEL_NAME = "fashion_MNIST_model_0.pt"
+MODEL_SAVE_PATH = MODEL_PATH / MODEL_NAME
+
+# 3. Save the model's state dict
+print(f"Saving the model to: {MODEL_SAVE_PATH}")
+torch.save(obj=model.state_dict(),
+           f=MODEL_SAVE_PATH)
+
+# Get random samples from the test data
+random.seed(42)
+test_samples = []
+test_labels = []
+for sample, label in random.sample(list(test_data), k=9):
+    test_samples.append(sample)
+    test_labels.append(label)
+
+# Make predictions
+pred_probs = make_predictions(model=model,
+                              data=test_samples)
+
+# Convert prediction probabilities to labels
+pred_labels = torch.argmax(pred_probs, dim=1)
+
+# Plot predictions
+plt.figure(figsize=(9, 9))
+nrows = 3
+ncols = 3
+for i, sample in enumerate(test_samples):
+    # Create subplot
+    plt.subplot(nrows, ncols, i + 1)
+
+    # Plot the target image
+    plt.imshow(sample.squeeze(), cmap="gray")
+
+    # Find the prediction (in text form)
+    pred_label = class_names[pred_labels[i]]
+
+    # Get the truth label
+    truth_label = class_names[test_labels[i]]
+
+    # Create a title for the plot
+    title_text = f"Pred: {pred_label} | Truth: {truth_label}"
+
+    # Check for equality between pred and truth and change the colour of title text
+    if pred_label == truth_label:
+        plt.title(title_text, fontsize=10, c="g")   # Green text if prediction is correct
+    else:
+        plt.title(title_text, fontsize=10, c="r")
+    plt.axis(False)
+plt.show()
+
+# 1. Make predicitons with trained model
+y_preds = []
+model.eval()
+with torch.inference_mode():
+    for X, y in tqdm(test_dataloader, desc="Making predictions..."):
+        # Send the data and targets to target device
+        X, y = X.to(device), y.to(device)
+        # Do the forward pass
+        y_logit = model(X)
+        # Turn predictions from logit -> pred probs -> pred labels
+        y_pred = torch.argmax(torch.softmax(y_logit.squeeze(), dim=0), dim=1)
+        # Put prediction on CPU for evaluation
+        y_preds.append(y_pred.cpu())
+
+# Concatenate list of predictions into a tensor
+y_pred_tensor = torch.cat(y_preds)
+
+# 2. Setup confusion matrix and compare predictions to targets
+confmat = ConfusionMatrix(num_classes=len(class_names),
+                          task='multiclass')
+confmat_tensor = confmat(preds=y_pred_tensor,
+                         target=test_data.targets)
+
+# 3. Plot the confusion matrix
+fig, ax = plot_confusion_matrix(
+    conf_mat=confmat_tensor.numpy(),    # matplotlib likes working with numpy
+    class_names=class_names,
+    figsize=(10, 7)
+)
+plt.show()
